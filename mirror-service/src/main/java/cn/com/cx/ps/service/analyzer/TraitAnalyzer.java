2cebded2fdc540de31c3263496228256cfc0ad6c
piggy <codingforkind@163.com>
2017-11-23 21:26:06 +0800

    rearrangement

--- mirror-service/src/main/java/cn/com/cx/ps/service/analyzer/TraitAnalyzer.java
+++ mirror-service/src/main/java/cn/com/cx/ps/service/analyzer/TraitAnalyzer.java
@@ -1,4 +1,26 @@
 package cn.com.cx.ps.service.analyzer;
 
+import cn.com.cx.ps.variable.CustomizedClass;
+
+import java.util.Map;
+import java.util.Set;
+
 public interface TraitAnalyzer {
+
+    /**
+     * @param prjDir absolute path for the project
+     * @return <p>key: java file path</p>
+     * <p>value: full package name</p>
+     */
+    public Map<String, String> analyzePackages(String prjDir);
+
+    /**
+     * @param javaFilePath absolute path for the java file
+     * @return <p>full package name for the java file</p>
+     */
+    public String getPackageName(String javaFilePath);
+
+    public Map<String, Set<CustomizedClass>> analyzeClasses(String prjDir);
+
+
 }
