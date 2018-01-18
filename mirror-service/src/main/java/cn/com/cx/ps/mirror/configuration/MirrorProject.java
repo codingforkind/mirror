8c033253f35e5f078c84e3e0517928f12c02ee60
piggy <codingforkind@163.com>
2018-01-18 08:06:26 +0800

    re

--- mirror-service/src/main/java/cn/com/cx/ps/mirror/configuration/MirrorProject.java
+++ mirror-service/src/main/java/cn/com/cx/ps/mirror/configuration/MirrorProject.java
@@ -25,102 +25,102 @@ import java.util.concurrent.Future;
 //@ConditionalOnClass(MirrorProjectProperties.class)
 @EnableConfigurationProperties(MirrorProjectProperties.class)
 public class MirrorProject {
-    private static Logger logger = LoggerFactory.getLogger(MirrorProject.class);
-    /**
-     * all java files in the project
-     */
-    private Set<String> prjJavaFiles = new HashSet<>();
-    /**
-     * the number of all java files
-     */
-    private int fileCount;
-    private MirrorProjectProperties properties;
+	private static Logger logger = LoggerFactory.getLogger(MirrorProject.class);
+	/**
+	 * all java files in the project
+	 */
+	private Set<String> prjJavaFiles = new HashSet<>();
+	/**
+	 * the number of all java files
+	 */
+	private int fileCount;
+	private MirrorProjectProperties properties;
 
+	private Map<String, ClassFile> prjClassesFile = new HashMap<>();
 
-    private Map<String, ClassFile> prjClassesFile = new HashMap<>();
+	private ExecutorService executorService = Executors.newFixedThreadPool(13);
 
-    private ExecutorService executorService = Executors.newFixedThreadPool(13);
-
-    public MirrorProject(MirrorProjectProperties mirrorProjectProperties) throws ProjectException {
-        this.properties = mirrorProjectProperties;
-        this.initjavaFiles(new File(this.properties.getPath()));
-    }
+	public MirrorProject(MirrorProjectProperties mirrorProjectProperties) throws ProjectException {
+		this.properties = mirrorProjectProperties;
+		this.initjavaFiles(new File(this.properties.getPath()));
+	}
 
 	// 重构，至此，继续Variable解析和存储变量
-    @PostConstruct
-    public void startFileAnalyzer() {
-//        initialized, start to analyze
-        FileAnalyzerRunner fileAnalyzer = new FileAnalyzerRunner(this.prjJavaFiles);
-        Future<?> fileSubmit = executorService.submit(fileAnalyzer);
-        if (fileSubmit.isDone()) {
-            PackageAnalyzerRunner packageAnalyzer = new PackageAnalyzerRunner(fileAnalyzer.getPrjCompUnits());
-            Future<?> pkgSubmit = executorService.submit(packageAnalyzer);
+	@PostConstruct
+	public void startFileAnalyzer() {
+		// initialized, start to analyze
+		FileAnalyzerRunner fileAnalyzer = new FileAnalyzerRunner(this.prjJavaFiles);
+		Future<?> fileSubmit = executorService.submit(fileAnalyzer);
+		if (fileSubmit.isDone()) {
+			PackageAnalyzerRunner packageAnalyzer = new PackageAnalyzerRunner(fileAnalyzer.getPrjCompUnits());
+			Future<?> pkgSubmit = executorService.submit(packageAnalyzer);
 
-            ClassAnalyzerRunner classAnalyzer = new ClassAnalyzerRunner(fileAnalyzer.getPrjCompUnits());
-            Future<?> clsSubmit = executorService.submit(classAnalyzer);
+			ClassAnalyzerRunner classAnalyzer = new ClassAnalyzerRunner(fileAnalyzer.getPrjCompUnits());
+			Future<?> clsSubmit = executorService.submit(classAnalyzer);
 
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+			// 等待包和类解析完毕
+			while (pkgSubmit.isDone() && clsSubmit.isDone()) {
+				VariableVisitor variableVisitor = null;
+				for (String tmpPath : prjJavaFiles) {
+					variableVisitor = new VariableVisitor(tmpPath, this);
 
-            if (pkgSubmit.isDone() && clsSubmit.isDone()) {
-                VariableVisitor variableVisitor = null;
-                for (String tmpPath : prjJavaFiles) {
-                    variableVisitor = new VariableVisitor(tmpPath, this);
+					ClassFile classFile = new ClassFile(tmpPath);
+					classFile.getCompilationUnit().accept(variableVisitor);
+					classFile.setVariablesInFile(variableVisitor.getVariables());
 
-                    ClassFile classFile = new ClassFile(tmpPath);
-                    classFile.getCompilationUnit().accept(variableVisitor);
-                    classFile.setVariablesInFile(variableVisitor.getVariables());
+					this.prjClassesFile.put(tmpPath, classFile);
+				}
+			}
+		}
 
-                    this.prjClassesFile.put(tmpPath, classFile);
-                }
-            }
-        }
+	}
 
-    }
+	public int getFileCount() {
+		return fileCount;
+	}
 
-    public int getFileCount() {
-        return fileCount;
-    }
+	public Map<String, ClassFile> getPrjClassesFile() {
+		return prjClassesFile;
+	}
 
-    public Map<String, ClassFile> getPrjClassesFile() {
-        return prjClassesFile;
-    }
+	public boolean classInProject(String qualifiedClassName) {
+		for (ClassFile classFile : this.prjClassesFile.values()) {
+			for (Class customizedClass : classFile.getClassesInFile()) {
+				if (customizedClass.getQualifiedName().contains(qualifiedClassName))
+					return true;
+				continue;
+			}
+		}
+		return false;
+	}
 
-    public boolean classInProject(String qualifiedClassName) {
-        for (ClassFile classFile : this.prjClassesFile.values()) {
-            for (Class customizedClass : classFile.getClassesInFile()) {
-                if (customizedClass.getQualifiedName().contains(qualifiedClassName)) return true;
-                continue;
-            }
-        }
-        return false;
-    }
+	public Set<String> getPrjJavaFiles() {
+		return prjJavaFiles;
+	}
 
-    public Set<String> getPrjJavaFiles() {
-        return prjJavaFiles;
-    }
-
-    /**
-     * <p>extract all the java file in the project and store it in the property of @see prjJavaFiles.</p>
-     * <p>get all files in this project ant the count of the files(".java")</p>
-     *
-     * @param dir
-     */
-    private void initjavaFiles(File dir) {
-        File[] fs = dir.listFiles();
-        String tmpPath = null;
-        for (int i = 0; i < fs.length; i++) {
-            if (fs[i].getAbsolutePath().endsWith(".java")) {
-                tmpPath = fs[i].getAbsolutePath();
-                this.fileCount += 1;
-                prjJavaFiles.add(tmpPath);
-            }
-            if (fs[i].isDirectory()) {
-                initjavaFiles(fs[i]);
-            } // end if
-        } // end for
-    }
+	/**
+	 * <p>
+	 * extract all the java file in the project and store it in the property of @see
+	 * prjJavaFiles.
+	 * </p>
+	 * <p>
+	 * get all files in this project ant the count of the files(".java")
+	 * </p>
+	 *
+	 * @param dir
+	 */
+	private void initjavaFiles(File dir) {
+		File[] fs = dir.listFiles();
+		String tmpPath = null;
+		for (int i = 0; i < fs.length; i++) {
+			if (fs[i].getAbsolutePath().endsWith(".java")) {
+				tmpPath = fs[i].getAbsolutePath();
+				this.fileCount += 1;
+				prjJavaFiles.add(tmpPath);
+			}
+			if (fs[i].isDirectory()) {
+				initjavaFiles(fs[i]);
+			} // end if
+		} // end for
+	}
 }
