6a9d7271c3282545d168ab0a9ddbe8072026029a
piggy <codingforkind@163.com>
2018-01-23 08:39:14 +0800

    re

---
+++ mirror-service/src/main/java/cn/com/cx/ps/mirror/project/service/ProjectInitializationService.java
@@ -0,0 +1,14 @@
+package cn.com.cx.ps.mirror.project.service;
+
+import cn.com.cx.ps.mirror.project.MirrorProject;
+
+public interface ProjectInitializationService {
+	
+	/**
+	 * Mirror project initialization
+	 * @param mirrorProject
+	 * @return
+	 */
+	public boolean initMirrorProject(MirrorProject mirrorProject);
+
+}
---
+++ mirror-service/src/main/java/cn/com/cx/ps/mirror/project/service/impl/ProjectInitializationServiceImpl.java
@@ -0,0 +1,18 @@
+package cn.com.cx.ps.mirror.project.service.impl;
+
+import org.springframework.stereotype.Component;
+
+import cn.com.cx.ps.mirror.project.MirrorProject;
+import cn.com.cx.ps.mirror.project.service.ProjectInitializationService;
+
+@Component
+public class ProjectInitializationServiceImpl implements ProjectInitializationService {
+
+	@Override
+	public boolean initMirrorProject(MirrorProject mirrorProject) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+}
