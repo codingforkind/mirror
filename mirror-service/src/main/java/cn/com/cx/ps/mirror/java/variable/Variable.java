8a99446444b77687c5db76442eed40c2390a9f29
piggy <codingforkind@163.com>
2018-03-03 16:40:40 +0800

    re

--- mirror-service/src/main/java/cn/com/cx/ps/mirror/java/variable/Variable.java
+++ mirror-service/src/main/java/cn/com/cx/ps/mirror/java/variable/Variable.java
@@ -1,5 +1,7 @@
 package cn.com.cx.ps.mirror.java.variable;
 
+import java.util.Objects;
+
 import org.eclipse.jdt.core.dom.ASTNode;
 
 import lombok.Data;
@@ -18,50 +20,22 @@ public class Variable {
 
     private int lineNum;
     private ASTNode astNode;
+    
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, variableType, isField, file);
+	}
 
     @Override
-    public int hashCode() {
-        int prime = 37;
-        int result = 1;
-        result += isField ? 0 : 1;
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null || getClass() != obj.getClass())
+			return false;
 
-        result += result * prime + ((name == null) ? 0 : name.hashCode());
-        result += result * prime + ((variableType == null) ? 0 : variableType.hashCode());
-        result += result * prime + ((file == null) ? 0 : file.hashCode());
-
-        return result;
-    }
-
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null) return false;
-        if (getClass() != obj.getClass()) return false;
-
-        Variable tmV = (Variable) obj;
-        if (null == name) {
-            if (tmV.getName() != null) return false;
-        } else {
-            if (!name.equals(tmV.getName())) return false;
-        }
-
-        if (null == variableType) {
-            if (tmV.getVariableType() != null) return false;
-        } else {
-            if (!variableType.equals(tmV.getVariableType())) return false;
-        }
-
-        if (isField) if (!tmV.isField()) return false;
-        if (!isField) if (tmV.isField()) return false;
-
-        if (null == file) {
-            if (null != tmV.getFile()) return false;
-        } else {
-            if (!file.equals(tmV.getFile())) return false;
-        }
-
-        return true;
-    }
+		Variable that = (Variable) obj;
+		return name == that.getName() && variableType == that.getVariableType() && isField == that.isField()
+				&& file == that.getFile();
+	}
 
 }
