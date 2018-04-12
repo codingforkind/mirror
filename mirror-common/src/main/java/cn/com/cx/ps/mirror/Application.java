ef1f810557c60fa41986a4452152295ed90c90c0
piggy <codingforkind@163.com>
2018-04-12 11:05:54 +0800

    re

--- mirror-common/src/main/java/cn/com/cx/ps/mirror/Application.java
+++ mirror-common/src/main/java/cn/com/cx/ps/mirror/Application.java
@@ -2,7 +2,7 @@ package cn.com.cx.ps.mirror;
 
 /**
  * @author Piggy
- *
+ * This is a common project for analysis, construct edges, program slicing etc.
  * @description 
  * @since 2018年4月12日
  */
