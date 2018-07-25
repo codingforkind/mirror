5deeecaca719d7c06e116f0661a507a4ef6821ee
piggy <codingforkind@163.com>
2018-07-25 10:49:56 +0800

    re

--- mirror-web/src/main/java/cn/com/cx/mirror/web/dao/mapper/CommonMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.com.cx.mirror.web.dao.mapper;
-
-import tk.mybatis.mapper.common.BaseMapper;
-import tk.mybatis.mapper.common.ConditionMapper;
-import tk.mybatis.mapper.common.Marker;
-import tk.mybatis.mapper.common.special.InsertListMapper;
-
-public interface CommonMapper<T> extends BaseMapper<T>, ConditionMapper<T>, InsertListMapper<T>, Marker {
-}
