549e5b9f96ed904f8e2a2c2687fc2174bdc93a03
piggy <codingforkind@163.com>
2018-07-25 14:19:31 +0800

    re

---
+++ mirror-common/src/main/java/cn/com/mirror/project/Archive.java
@@ -0,0 +1,23 @@
+package cn.com.mirror.project;
+
+import cn.com.mirror.variable.Class;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author piggy
+ * @description
+ * @date 18-7-25
+ */
+@Data
+public class Archive implements Serializable {
+
+    private String path; // archive's location
+    private Set<String> targets;
+
+    private Map<String, Set<Class>> classes; // all classes in this java archive
+
+}
