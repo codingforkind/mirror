5058c1b879b442d2392267a770898aeb45673f73
piggy <codingforkind@163.com>
2018-07-25 14:54:27 +0800

    re

---
+++ mirror-common/src/main/java/cn/com/mirror/handler/impl/DefaultAnalyzeHandler.java
@@ -0,0 +1,18 @@
+package cn.com.mirror.handler.impl;
+
+import cn.com.mirror.handler.AnalyzeHandler;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author piggy
+ * @description
+ * @date 18-7-25
+ */
+@Slf4j
+public class DefaultAnalyzeHandler implements AnalyzeHandler {
+
+    @Override
+    public void run() {
+
+    }
+}
