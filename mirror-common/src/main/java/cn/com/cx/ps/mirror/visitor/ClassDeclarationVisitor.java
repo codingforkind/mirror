2e9b60eb600840a84410700a9fb5b6382b024cb8
piggy <codingforkind@163.com>
2018-07-25 11:39:55 +0800

    re

--- mirror-common/src/main/java/cn/com/cx/ps/mirror/visitor/ClassDeclarationVisitor.java
+++ mirror-common/src/main/java/cn/com/cx/ps/mirror/visitor/ClassDeclarationVisitor.java
@@ -3,9 +3,11 @@ package cn.com.cx.ps.mirror.visitor;
 import java.util.HashSet;
 import java.util.Set;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.PackageDeclaration;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,42 +18,50 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Slf4j
 public class ClassDeclarationVisitor extends ASTVisitor {
-	private Logger log = LoggerFactory.getLogger(getClass());
-	private String file;
-	// the first element in this list is the outer class, others are inner class in the first class.
-	private Set<Class> prjClasses = new HashSet<>();
+    private String packageName;
+    // the first element in this list is the outer class, others are inner class in the first class.
+    private Set<Class> prjClasses = new HashSet<>();
 
-	public ClassDeclarationVisitor(String file) {
-		this.file = file;
-	}
+    private String file;
 
-	@Override
-	public boolean visit(TypeDeclaration node) {
-		ITypeBinding typeBinding = node.resolveBinding();
-		if (null != typeBinding) {
-			// log.info("class name: {}", typeBinding.getQualifiedName());
-			Class mirrorClass = new Class();
-			mirrorClass.setFile(this.file);
-			mirrorClass.setInterface(node.isInterface());
-			mirrorClass.setName(node.getName().getIdentifier());
-			mirrorClass.setTypeDeclaration(node);
-			mirrorClass.setQualifiedName(typeBinding.getQualifiedName());
+    public ClassDeclarationVisitor(String file) {
+        this.file = file;
+    }
 
-			if (null != typeBinding.getPackage()) {
-				if (!StringUtils.isEmpty(typeBinding.getPackage().getName())) {
-					mirrorClass.setPackageName(typeBinding.getPackage().getName());
-				} else {
-					mirrorClass.setPackageName(null);
-				}
-			}
+    @Override
+    public boolean visit(PackageDeclaration node) {
+        packageName = node.getName().getFullyQualifiedName();
+        return super.visit(node);
+    }
 
-			this.prjClasses.add(mirrorClass);
-		} else {
-			log.error("NULL resolve binding for node: ~{}~", typeBinding);
-		}
+    @Override
+    public boolean visit(TypeDeclaration node) {
+        ITypeBinding typeBinding = node.resolveBinding();
+        if (null != typeBinding) {
+            // log.info("class name: {}", typeBinding.getQualifiedName());
+            Class mirrorClass = new Class();
+            mirrorClass.setFile(this.file);
+            mirrorClass.setInterface(node.isInterface());
+            mirrorClass.setName(node.getName().getIdentifier());
+            mirrorClass.setTypeDeclaration(node);
+            mirrorClass.setQualifiedName(typeBinding.getQualifiedName());
 
-		return super.visit(node);
-	}
+            if (null != typeBinding.getPackage()) {
+                if (!StringUtils.isEmpty(typeBinding.getPackage().getName())) {
+                    mirrorClass.setPackageName(typeBinding.getPackage().getName());
+                } else {
+                    mirrorClass.setPackageName(null);
+                }
+            }
+
+            this.prjClasses.add(mirrorClass);
+        } else {
+            log.error("NULL resolve binding for node: ~{}~", typeBinding);
+        }
+
+        return super.visit(node);
+    }
 
 }
