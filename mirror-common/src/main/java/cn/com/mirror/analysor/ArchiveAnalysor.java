28937114bf81eb2c8478cb76a028a9db4a2d05c3
piggy <codingforkind@163.com>
2018-07-25 16:42:06 +0800

    re

---
+++ mirror-common/src/main/java/cn/com/mirror/analysor/ArchiveAnalysor.java
@@ -0,0 +1,18 @@
+package cn.com.mirror.analysor;
+
+import cn.com.mirror.project.Archive;
+
+/**
+ * @author piggy
+ * @description
+ * @date 18-7-25
+ */
+public class ArchiveAnalysor {
+
+    public Archive targetAnalyze(){
+        Archive archive = new Archive();
+
+        return archive;
+    }
+
+}
---
+++ mirror-common/src/main/java/cn/com/mirror/project/factory/ArchiveFactory.java
@@ -0,0 +1,14 @@
+package cn.com.mirror.project.factory;
+
+import cn.com.mirror.project.Archive;
+
+/**
+ * @author piggy
+ * @description
+ * @date 18-7-25
+ */
+public interface ArchiveFactory {
+
+    Archive archive();
+    
+}
