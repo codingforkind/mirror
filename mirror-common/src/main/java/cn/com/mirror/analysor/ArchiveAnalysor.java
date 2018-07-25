8b50e57482aef6fff2c20e11483a8e5d12ea4ee2
piggy <codingforkind@163.com>
2018-07-25 17:58:15 +0800

    re

--- mirror-common/src/main/java/cn/com/mirror/analysor/ArchiveAnalysor.java
+++ mirror-common/src/main/java/cn/com/mirror/analysor/ArchiveAnalysor.java
@@ -4,6 +4,7 @@ import cn.com.mirror.project.Archive;
 import cn.com.mirror.utils.AstUtils;
 import cn.com.mirror.utils.FileUtils;
 import cn.com.mirror.visitor.ClassDeclarationVisitor;
+import cn.com.mirror.visitor.VariableVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 
 /**
@@ -20,10 +21,18 @@ public class ArchiveAnalysor {
 
         for (String targetPath : archive.getTargets()) {
             CompilationUnit compilationUnit = AstUtils.getCompUnitResolveBinding(targetPath);
+
+            // packages/classes analysis
             ClassDeclarationVisitor classDeclarationVisitor = new ClassDeclarationVisitor(targetPath);
             compilationUnit.accept(classDeclarationVisitor);
-
+            archive.addPackages(targetPath, classDeclarationVisitor.getPackageName());
             archive.addClasses(targetPath, classDeclarationVisitor.getClsSet());
+
+
+            // variable analysis
+            VariableVisitor variableVisitor = new VariableVisitor(targetPath,
+                    archive.getPackages().get(targetPath), archive.getClasses());
+            compilationUnit.accept(variableVisitor);
         }
 
         return archive;
--- mirror-common/src/main/java/cn/com/mirror/project/Archive.java
+++ mirror-common/src/main/java/cn/com/mirror/project/Archive.java
@@ -1,6 +1,7 @@
 package cn.com.mirror.project;
 
 import cn.com.mirror.variable.Class;
+import cn.com.mirror.variable.Variable;
 import lombok.Data;
 import org.apache.commons.lang3.Validate;
 
@@ -20,17 +21,41 @@ public class Archive implements Serializable {
     private String path; // archive's location
     private Set<String> targets; // target files in the archive, such as java file in a java project.
 
+    private Map<String, String> packages; // all packages in this java archive
     private Map<String, Set<Class>> classes; // all classes in this java archive
+    private Map<String, Set<Variable>> variables; // all variables in this java archive
 
 
     public void addClasses(String targetPath, Set<Class> clsSet) {
-        Validate.notEmpty(targetPath, "Target's path can not be null.");
+        Validate.notEmpty(targetPath, "Target's path can not be empty.");
         Validate.notEmpty(clsSet, "Target has none classes.");
         if (null != classes) {
             classes.put(targetPath, clsSet);
         } else {
             classes = new HashMap<>();
             classes.put(targetPath, clsSet);
+        }
+    }
+
+    public void addPackages(String targetPath, String packageName) {
+        Validate.notEmpty(targetPath, "Target's path can not be empty.");
+        Validate.notEmpty(packageName, "Package name can not be empty.");
+        if (null != packages) {
+            packages.put(targetPath, packageName);
+        } else {
+            packages = new HashMap<>();
+            packages.put(targetPath, packageName);
+        }
+    }
+
+    public void addVariables(String targetPath, Set<Variable> variableSet) {
+        Validate.notEmpty(targetPath, "Target's path can not be empty.");
+        Validate.notEmpty(variableSet, "Target has none variables.");
+        if (null != variables) {
+            variables.put(targetPath, variableSet);
+        } else {
+            variables = new HashMap<>();
+            variables.put(targetPath, variableSet);
         }
     }
 }
