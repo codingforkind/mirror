b82494b7e1d2ae1cd0f88e6119fc412bf169735b
piggy <codingforkind@163.com>
2018-08-08 16:32:32 +0800

    re

---
+++ mirror-common/src/test/cn/com/mirror/graph/Neo4jDriverTests.java
@@ -0,0 +1,17 @@
+package cn.com.mirror.graph;
+
+import cn.com.mirror.graph.storage.neo4j.driver.Neo4jDriver;
+import org.junit.Test;
+
+/**
+ * @author piggy
+ * @description
+ * @date 18-8-8
+ */
+public class Neo4jDriverTests {
+    @Test
+    public void testDriver() {
+        Neo4jDriver neo4jDriver = new Neo4jDriver();
+        neo4jDriver.example("hello");
+    }
+}
