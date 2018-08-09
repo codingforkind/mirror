adf23365926614b8d76dc3fe6e5467b91a95b49e
piggy <codingforkind@163.com>
2018-08-09 18:19:53 +0800

    re

--- mirror-common/src/main/java/cn/com/mirror/graph/neo4j/driver/Neo4jSessionFactory.java
+++ mirror-common/src/main/java/cn/com/mirror/graph/neo4j/driver/Neo4jSessionFactory.java
@@ -2,6 +2,7 @@ package cn.com.mirror.graph.neo4j.driver;
 
 import cn.com.mirror.graph.neo4j.config.Neo4jProperty;
 import org.neo4j.ogm.config.Configuration;
+import org.neo4j.ogm.session.Session;
 import org.neo4j.ogm.session.SessionFactory;
 
 /**
@@ -11,12 +12,9 @@ import org.neo4j.ogm.session.SessionFactory;
  */
 public class Neo4jSessionFactory {
     private final SessionFactory sessionFactory;
+    private final Neo4jSessionFactory neo4jSessionFactory = new Neo4jSessionFactory(Neo4jProperty.newInstance());
 
-    public Neo4jSessionFactory() {
-        this(Neo4jProperty.newInstance());
-    }
-
-    public Neo4jSessionFactory(Neo4jProperty neo4jProperty) {
+    private Neo4jSessionFactory(Neo4jProperty neo4jProperty) {
         Configuration configuration = new Configuration.Builder()
                 .uri(neo4jProperty.getServAddr())
                 .credentials(neo4jProperty.getUsername(), neo4jProperty.getPassword())
@@ -24,4 +22,14 @@ public class Neo4jSessionFactory {
 
         this.sessionFactory = new SessionFactory(configuration, "com.mycompany.app.domainclasses");
     }
+
+    public Neo4jSessionFactory getFactory() {
+        return neo4jSessionFactory;
+    }
+
+    public Session newSession() {
+        return sessionFactory.openSession();
+    }
+
+
 }
