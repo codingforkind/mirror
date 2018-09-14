b49e8a90e8ebeec17f659dc49940bebe10458ac6
codingforkind@163.com <Damonx521___>
2018-09-14 14:03:03 +0800

    re

--- mirror-examples/mirror-web/src/main/java/cn/com/mirror/project/dao/mapper/ArchiveMapper.java
+++ mirror-examples/mirror-web/src/main/java/cn/com/mirror/project/dao/mapper/ArchiveMapper.java
@@ -2,6 +2,14 @@ package cn.com.mirror.project.dao.mapper;
 
 import cn.com.mirror.common.PgCommonMapper;
 import cn.com.mirror.project.dao.entity.Archive;
+import org.apache.ibatis.annotations.InsertProvider;
+import org.apache.ibatis.annotations.Options;
+import tk.mybatis.mapper.provider.base.BaseInsertProvider;
 
 public interface ArchiveMapper extends PgCommonMapper<Archive> {
+
+    @Override
+    @Options(keyProperty = "achvId", useGeneratedKeys = true)
+    @InsertProvider(type = BaseInsertProvider.class,method = "dynamicSQL")
+    int insertSelective(Archive archive);
 }
